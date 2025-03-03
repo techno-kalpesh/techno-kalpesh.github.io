5b3faebef05390e2945a275c8a34bc5828b884fa
Kalpesh Kurbetti <130686300+techno-kalpesh@users.noreply.github.com>
2025-03-03 10:05:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-    const currentTheme = document.body.getAttribute("data-theme");
-    if (currentTheme === "dark") {
-        document.body.removeAttribute("data-theme");
-    } else {
-        document.body.setAttribute("data-theme", "dark");
-    }
-}
