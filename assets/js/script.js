36180517c0067bbac5df525c31d2cd9f3d019f97
Kalpesh Kurbetti <130686300+techno-kalpesh@users.noreply.github.com>
2025-03-03 10:32:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
-function toggleTheme() {
+const toggleButton = document.getElementById("theme-toggle");
+
+toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
-}
+    document.querySelector(".main-menu").classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        toggleButton.textContent = "☀️";
+    } else {
+        toggleButton.textContent = "🌙";
+    }
+});
